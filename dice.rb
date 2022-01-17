2ce4bd9a36a27adb8eacd16f5f9c86eedd265763
Razr <example@mail.ru>
2022-01-17 16:37:43 +0300

    version 2.0

--- dice.rb
+++ dice.rb
@@ -1,5 +1,14 @@
 puts "How many dices?"
 num = gets.to_i
+
+def show_rolling_die
+  100.times do
+    # Вместо puts используем print, т.к. он не добавляет перенос строки в конце
+    print "#{rand(6) + 1}\r"
+    sleep 0.01
+  end
+end
+show_rolling_die
 puts "<=================>"
 num.times do
   puts rand(6) + 1
